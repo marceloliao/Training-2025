b3154067e85bd2bd4b9dafc525a6da3b72cd51f2
marceloliao <marceloliao@hotmail.come>
2025-11-09 21:05:57 -0500

    Adding all .vs subfolders to .gitignore file

--- .NET Core/C_Sharp_Reflection/obj/Debug/net8.0/C_Sharp_Reflection.AssemblyInfo.cs
+++ .NET Core/C_Sharp_Reflection/obj/Debug/net8.0/C_Sharp_Reflection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C_Sharp_Reflection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3086218c19a70b64990529dc0096d8f6c7f075")]
 [assembly: System.Reflection.AssemblyProductAttribute("C_Sharp_Reflection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C_Sharp_Reflection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
